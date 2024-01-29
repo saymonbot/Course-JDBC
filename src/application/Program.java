8e6705a8417b169d4a1668565617e62d18ded350
Saymon Botelho <saymon_botelho123@outlook.com>
2024-01-29 15:56:18 -0300

    DELETE

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,6 +10,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 import db.DB;
+import db.DbIntegrityException;
 
 public class Program {
 
@@ -19,24 +20,19 @@ public class Program {
 		
 		Connection conn = null;
 		PreparedStatement pst = null;
-	//	Statement st = null;
 		ResultSet rs = null;
 		
 		try {
 			conn = DB.getConnection();
-					
+			
 			pst = conn.prepareStatement(
-					"INSERT INTO seller "
-					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
+					"INSERT INTO Department "
+					+ "(Name)"
 					+ "VALUES "
-					+ "(?, ?, ?, ?, ?)",
+					+ "(?)",
 					Statement.RETURN_GENERATED_KEYS);
 			
-			pst.setString(1, "Carl Purple");
-			pst.setString(2, "carl@gmail.com");
-			pst.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
-			pst.setDouble(4, 3000.0);
-			pst.setInt(5, 4);
+			pst.setString(1, "D1");
 			
 			int rowsAffected = pst.executeUpdate();
 			
@@ -46,10 +42,44 @@ public class Program {
 					int id = rs.getInt(1);
 					System.out.println("Done! Id = " + id);
 				}
-			}else {
-				System.out.println("No rows affected!");
+			}
+		}catch (SQLException e) {
+				
+		}
+					
+			try {
+				pst = conn.prepareStatement(
+						"INSERT INTO seller "
+						+ "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
+						+ "VALUES "
+						+ "(?, ?, ?, ?, ?)",
+						Statement.RETURN_GENERATED_KEYS);
+				
+				pst.setString(1, "Carl Purple");
+				pst.setString(2, "carl@gmail.com");
+				pst.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
+				pst.setDouble(4, 3000.0);
+				pst.setInt(5, 4);
+				
+				int rowsAffected = pst.executeUpdate();
+				
+				if(rowsAffected > 0) {
+					rs = pst.getGeneratedKeys();
+					while (rs.next()) {
+						int id = rs.getInt(1);
+						System.out.println("Done! Id = " + id);
+					}
+				}else {
+					System.out.println("No rows affected!");
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} catch (ParseException e) {
+				e.printStackTrace();
 			}
 			
+			
+		try {
 			pst = conn.prepareStatement(
 				"UPDATE seller "
 				+ "SET BaseSalary = BaseSalary + ? "
@@ -59,22 +89,27 @@ public class Program {
 			pst.setDouble(1, 200.0);
 			pst.setInt(2, 2);
 			
+			int rowsAffected = pst.executeUpdate();
+		
+			System.out.println("Done! Rows Affected: " + rowsAffected);
+			
+			
+			pst = conn.prepareStatement(
+				"DELETE from department "
+				+ "WHERE "
+				+ "Name = 'D1' ");
+
 			rowsAffected = pst.executeUpdate();
 		
 			System.out.println("Done! Rows Affected: " + rowsAffected);
 			
-		//	rs = st.executeQuery("select * from department");
-			
 
 		} catch(SQLException e) {
-			e.printStackTrace();
-		} catch(ParseException e) {
-			e.printStackTrace();
+			throw new DbIntegrityException(e.getMessage());
 		}
 		finally {
 			DB.closeStatement(pst);
 			DB.closeResultSet(rs);
-		//	DB.closeStatement(st);
 			DB.closeConnection();
 		}
 	}
